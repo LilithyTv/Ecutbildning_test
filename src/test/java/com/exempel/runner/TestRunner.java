8315f08c27a0f504611784aa03fef292d5d49217
Lily <116434991+LilithyTv@users.noreply.github.com>
2025-02-02 20:31:46 +0100

    Create TestRunner.java

---
+++ src/test/java/com/exempel/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package com.exempel.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"com.exempel.registration.steps"},
+    plugin = {"pretty", "html:target/cucumber-reports"},
+    monochrome = true
+)
+public class TestRunner {
+
+}
